94ef2151e49ca368f7266b47ff7683ea59bb2be4
AnAppleCore <994164148@qq.com>
2025-01-17 16:13:01 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
-# python train.py \
-#     --datasets_folder noisy_shrink_train \
-#     --n_epochs 20 \
-#     --GPU 0,1 \
-#     --train_datasets_size 6000  \
-#     --patch_x 80 \
-#     --patch_t 128
+python train.py \
+    --datasets_folder sleep_train \
+    --n_epochs 20 \
+    --GPU 0 \
+    --train_datasets_size 6000  \
+    --patch_x 128 \
+    --patch_t 128
 
 python test.py \
-    --datasets_folder noisy_shrink_train \
-    --denoise_model noisy_shrink_train_202407181911 \
-    --GPU 4,5 \
-    --patch_x 80 \
-    --patch_t 128
+    --datasets_folder sleep \
+    --denoise_model sleep_train_202411281859 \
+    --GPU 0 \
+    --patch_x 128 \
+    --patch_t 128 > sleep.log 2>&1 &
